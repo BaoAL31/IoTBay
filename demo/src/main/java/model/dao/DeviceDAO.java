110299c11b0ede6cd7fea596b8568905b3a8885b
Mahidi09876 <mahidirasanjana.ilangarathna@student.uts.edu.au>
2025-05-21 01:55:45 +1000

    Formatting fixes

--- demo/src/main/java/model/dao/DeviceDAO.java
+++ demo/src/main/java/model/dao/DeviceDAO.java
@@ -10,7 +10,7 @@ import java.util.List;
 import model.Device;
 
 public class DeviceDAO {
-    private Connection connection;
+    private final Connection connection;
 
     public DeviceDAO(Connection connection) {
         this.connection = connection;
